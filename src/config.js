8d51efe4ca80775082db21cc1f94ea29d8e273c6
Francisco <fluanc567@gmail.com>
2020-09-01 22:02:00 -0500

    Base

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const mongoose= require('mongoose');
-
-mongoose.connect('localhost://mongodb/prueba')
+module.exports = {
+    secret: 'mitokensecreto'
+};
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,30 +1,70 @@
 const { Router } = require('express');
 const router= Router();
 
-require('jsonwebtoken');
+const jwt= require('jsonwebtoken');
 
 const User= require('../models/user');
 
+const config = require('../config');
+
+const verifyToken= require('../controllers/verifyToken')
+//registrarse
 router.post('/registro', async (req,res,next)=>{
     //res.json('kha?');
     const {username,email,password}=req.body;
     const user= new User({
-        username: username,
-        email:email,
-        password:password
+        username,
+        email,
+        password
     });
     user.password= await user.encryptPassword(user.password);
     await user.save();
+     
+    /*JSON web Token*/
+    const token= jwt.sign({id: user._id}, config.secret,{
+        expiresIn: 60 * 60 * 24//un dia en segundos
+    })
+
     console.log(user)
-    res.json({message:'Received'});
+    res.json({auth: true,token});
 });
 
-router.post('/login', (req,res,next)=>{
-    res.json('login');
+//Loguearse
+router.post('/login', async (req,res,next)=>{
+    const {email,password} = (req.body);
+    const user = await User.findOne({email: email});
+
+    if (!user) {
+        return res.status(404).send("no existe el usuario");
+    }
+     
+    //Valdiar usuario
+    const passwordValido=await user.validatePassword(password);
+    if (!passwordValido) {
+        return res.status(401).json({auth: false,token: null});
+        
+    }
+    console.log(passwordValido)
+    
+    //asignarle un token de acceso
+    const token = jwt.sign({id: user._id},config.secret, {
+        expiresIn: 60 * 60 * 24
+    });
+
+    res.json({auth: true,token});
 });
 
-router.get('/perfil', (req,res,next)=>{
-    res.json('perfil');
+//Navegar por la aplicacion
+router.get('/perfil', verifyToken,async  (req,res,next)=>{
+    
+    //para no devolder un dato solo se pone {dato:0 }
+    const user= await User.findById(req.userId, {password:0 });
+
+    if (!user) {
+       return res.status(404).send("No existe el usuario")        
+    }
+
+    res.json(user)
 });
 
 module.exports= router;
